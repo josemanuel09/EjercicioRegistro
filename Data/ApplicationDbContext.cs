3e718802a51f6f472d9e850aefa77fa30a534676
josemanuel09 <138628459+josemanuel09@users.noreply.github.com>
2023-09-04 19:18:46 -0400

    EjercicioRegistro

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<Prioridad> Prioridades { get; set; }
+}
---
+++ Models/Prioridad.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+public class Prioridad
+{
+    public int PrioridadId { get; set; }
+
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    [StringLength(50, ErrorMessage = "La descripción no debe tener más de 50 caracteres.")]
+    [Remote(action: "IsDescripcionUnica", controller: "Prioridades", AdditionalFields = nameof(PrioridadId))]
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Los días de compromiso son obligatorios.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Los días de compromiso deben ser un número mayor que 0.")]
+    public int DiasCompromiso { get; set; }
+}
